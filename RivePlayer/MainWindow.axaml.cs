88d65b17d18e092dedcc0b7623ce31bdc3e94329
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-10 23:33:12 +0100

    Update MainWindow.axaml.cs

--- RivePlayer/MainWindow.axaml.cs
+++ RivePlayer/MainWindow.axaml.cs
@@ -7,8 +7,15 @@ namespace RivePlayer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoaded()
+        {
+            base.OnLoaded();
             
-            // this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/2244-4463-animated-login-screen.riv";
+
+            //this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/2244-4463-animated-login-screen.riv";
+            this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/3466-7249-3d-cube-demo.riv";
         }
     }
 }
