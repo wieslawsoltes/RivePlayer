4cc58892a80425b26db439f2c3ed5fd6ae674e2b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-10 23:42:02 +0100

    Update MainWindow.axaml.cs

--- RivePlayer/MainWindow.axaml.cs
+++ RivePlayer/MainWindow.axaml.cs
@@ -12,10 +12,10 @@ namespace RivePlayer
         protected override void OnLoaded()
         {
             base.OnLoaded();
-            
 
             //this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/2244-4463-animated-login-screen.riv";
-            this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/3466-7249-3d-cube-demo.riv";
+            //this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/3466-7249-3d-cube-demo.riv";
+            this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/3375-7092-the-pumpking-army.riv";
         }
     }
 }
